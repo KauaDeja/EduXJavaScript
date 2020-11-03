fad7a38e44bb166e28ee2dc7a05fc2ec803932da
Gabriel <gabriel.gaspargbg@gmail.com>
2020-11-03 18:09:43 -0300

    jsx Objetivo finalizado

--- edux-react/src/pages/objetivo/index.jsx
+++ edux-react/src/pages/objetivo/index.jsx
@@ -1,17 +1,55 @@
 import React from 'react';
+import {Carousel,Jumbotron} from 'react-bootstrap'
+import educacao1 from '../../assets/img/educacao3.jpg'
+import educacao2 from '../../assets/img/educacao2.jpg'
 import Menu from '../../components/menu/';
 import Rodape from '../../components/rodape/';
-import Titulo from '../../components/titulo/';
+import './index.css'
 
 const Objetivo = () =>{
     return(
-        <div>
+        <div >
             <Menu />
-            <Titulo titulo ="Objetivos" chamada ={<h2> Atribua e gerencie os objetivos de seus alunos</h2>}/>
+            <div id="Carrossel">
+            <Carousel>
+                <Carousel.Item>
+                    <img 
+                    className='d-block w-100'
+                    src={educacao1}
+                    alt='first-slide'
+                    />
+                    <Carousel.Caption>
+                        <div className='text'>
+                        <h1>Objetivos</h1>
+                        <h2>Visualize, gerencie ou atribua Objetivos</h2>
+                        </div>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img 
+                    className='d-block w-100'
+                    src={educacao2}
+                    alt='second-slide'
+                    />
+                    <Carousel.Caption>
+                        <div className='text'>
+                        <h1>Objetivos</h1>
+                        <h2>Visualize, gerencie ou atribua Objetivos</h2>
+                        </div>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+            </div>
+
+            <Jumbotron style={{marginBottom:'0rem'}}>
             <div className="container">
                    
                     <div className="bd-example" >
                         <form id="formObjetivo">
+                        <div className="form-group">
+                                <label htmlFor="Aluno"></label>
+                                <input type="text" className="form-control"  id="aluno" aria-describedby="Aluno" placeholder="Digite o nome do aluno"/>
+                        </div>
                            <div className="form-group">
                                 <label htmlFor="Turma"></label>
                                 <input type="text" className="form-control"  id="turma" aria-describedby="turma" placeholder="Informe a turma desejada"/>
@@ -38,6 +76,7 @@ const Objetivo = () =>{
                             <thead>
                                 <tr>
                                     <th scope="col">#</th>
+                                    <th scope="col">Aluno</th>
                                     <th scope="col">Turma</th>
                                     <th scope="col">Descrição</th>
                                     <th scope="col">Categoria</th>
@@ -49,8 +88,10 @@ const Objetivo = () =>{
                         </table>
                     </div>
                 </div>
+                </Jumbotron>
                 <Rodape/>
             </div>
+
             
     )
 } 
