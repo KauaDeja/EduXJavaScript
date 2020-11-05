afeb8a07d42b617cf8f88921d6f0de8af8648bb6
Gabriel <gabriel.gaspargbg@gmail.com>
2020-11-05 11:42:20 -0300

    integrando api

--- edux-react/src/pages/objetivo/index.jsx
+++ edux-react/src/pages/objetivo/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {Carousel,Jumbotron} from 'react-bootstrap'
 import educacao1 from '../../assets/img/educacao3.jpg'
 import educacao2 from '../../assets/img/educacao2.jpg'
@@ -6,12 +6,19 @@ import Menu from '../../components/menu/';
 import Rodape from '../../components/rodape/';
 import './index.css'
 
+
 const Objetivo = () =>{
+  
+  const [aluno , setAluno] = useState('');
+  const [turma , setTurma] = useState('');
+  const [descricao , setDescricao] = useState('');
+  const [categoria , setCategoria] = useState('');
+  const [nota , setNota] = useState('');
+  
+  const Salvar = () =>{
+    fetch
+  }
 
-    fetch('https://localhost:44305/api/Objetivo , https://localhost:44305/api/ObjetivoAluno',{
-        method:'POST'
-
-    })
     return(
         <div >
             <Menu />
@@ -53,24 +60,24 @@ const Objetivo = () =>{
                         <form id="formObjetivo">
                         <div className="form-group">
                                 <label htmlFor="Aluno"></label>
-                                <input type="text" className="form-control"  id="aluno" aria-describedby="Aluno" placeholder="Digite o nome do aluno"/>
+                                <input type="text" className="form-control" value={aluno} onChange={event => setAluno(event.target.value)}  id="aluno" aria-describedby="Aluno" placeholder="Digite o nome do aluno"/>
                         </div>
                            <div className="form-group">
                                 <label htmlFor="Turma"></label>
-                                <input type="text" className="form-control"  id="turma" aria-describedby="turma" placeholder="Informe a turma desejada"/>
+                                <input type="text" className="form-control" value={turma } onChange={event => setTurma(event.target.value)} id="turma" aria-describedby="turma" placeholder="Informe a turma desejada"/>
                             </div>
                             <div className="form-group">
                                 <label htmlFor="Descrição"></label>
-                                <input type="text" className="form-control"  id="descricao" aria-describedby="descricao" placeholder="Descreva o objetivo"/>
+                                <input type="text" className="form-control" value={descricao} onChange={event => setDescricao(event.target.value)} id="descricao" aria-describedby="descricao" placeholder="Descreva o objetivo"/>
                             </div>
                             <div className="form-group">
                                 <label htmlFor="Categoria"></label>
-                                <input type="text" className="form-control"  id="Categoria" 
+                                <input type="text" className="form-control" value={categoria} onChange={event => setCategoria(event.target.value)} id="Categoria" 
                                  placeholder="Informe a categoria atráves de seu ID (Critico - Desejável - Oculto)"/>
                             </div>
                             <div className="form-group">
                                 <label htmlFor="nota"></label>
-                                <input type="text" className="form-control small" id="nota" 
+                                <input type="text" className="form-control small" value={nota} onChange={event => setNota(event.target.value)} id="nota" 
                                 placeholder="Digite a nota atribuída ao aluno"/>
                             </div>
                             <button type="button" className="btn btn-secondary">Cancelar</button>
